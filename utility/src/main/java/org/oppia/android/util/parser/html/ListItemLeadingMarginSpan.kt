061e0a7ade64b29a79905d65129e7c0784cd9ac1
TanishMoral11 <ishantmoral@gmail.com>
2024-12-16 13:35:25 +0530

    Remove Ununsed Variable

--- utility/src/main/java/org/oppia/android/util/parser/html/ListItemLeadingMarginSpan.kt
+++ utility/src/main/java/org/oppia/android/util/parser/html/ListItemLeadingMarginSpan.kt
@@ -3,7 +3,6 @@ package org.oppia.android.util.parser.html
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Rect
 import android.graphics.RectF
 import android.text.Layout
 import android.text.Spanned
